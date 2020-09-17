b7350b10f9c54506ac6d077c702e89128aa1d29f
Agustin Torra <torra.agustinignacio@gmail.com>
2020-09-17 14:05:37 -0300

    🎨 Make Post h1 tag beautiful

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import * as React from "react";
 import "./styles.css";
 import PostsContainer from "./components/posts/PostsContainer";
 import SelectedPost from "./components/posts/SelectedPost";
+import colors from "./colors";
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Posts</h1>
-      <PostsContainer />
-      <SelectedPost />
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1 style={{ color: colors.primary }}>Posts</h1>
+			<PostsContainer />
+			<SelectedPost />
+		</div>
+	);
 };
 
 export default App;
